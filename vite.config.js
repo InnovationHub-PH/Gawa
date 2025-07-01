34bac892f1e72a49bcae18be7f4cd0edc282e6e6
InnovationHub-PH <makewithconfidence@gmail.com>
2025-07-01 02:02:56 -0700

    Fix Netlify deployment for multiple pages

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        search: resolve(__dirname, 'search.html'),
+        resources: resolve(__dirname, 'resources.html'),
+        post: resolve(__dirname, 'post.html'),
+        profile: resolve(__dirname, 'profile.html')
+      }
+    }
+  }
+})
